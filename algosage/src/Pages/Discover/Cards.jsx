96419ad364f13ac3ce6d0e8f3de86e2c05b8b3ce
lawrenceokim <lawrence4okim@gmail.com>
2023-10-11 10:01:10 +0100

    browse exchanges updated

--- algosage/src/Pages/Discover/Cards.jsx
+++ algosage/src/Pages/Discover/Cards.jsx
@@ -6,13 +6,13 @@ export const Cards = () => {
           className="absolute top-5 right-8 mr-2  w-28 h-32 hidden lg:block"
           src="https://app.gamma.xyz/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fstake-gamma-card-bg.fff15eaf.png&w=128&q=75"
         />
-        <h1 className="text-xl p-1 text-white/80">Stake GAMMA</h1>
-        <p className="text-sm text-gray-500 p-1 pb-6">
-          Stake GAMMA and receives fee distributions from protocol <br />
+        <h1 className="text-xl p-1 text-white/80">Stake ALGOSAGE</h1>
+        <p className="text-sm text-light p-1 pb-6">
+          Stake ALGOSAGE and receives fee distributions from protocol <br />
           revenue
         </p>
         <button className="font-medium flex items-center justify-center bg-light/20 text-white enabled:hover:bg-light/40 px-3 py-[11px] text-sm rounded-lg gap-x-2">
-          Stake GAMMA →{" "}
+          Stake ALGOSAGE →{" "}
         </button>
       </div>
 
@@ -24,7 +24,7 @@ export const Cards = () => {
         <h1 className="text-xl p-1 text-white/80">
           For Web3 Projects & Founders
         </h1>
-        <p className="text-sm text-gray-500 p-1 pb-6">
+        <p className="text-sm text-light p-1 pb-6">
           We implement automated strategies tailored to the <br />
           needs of your project
         </p>
